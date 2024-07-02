ac54eba8526b539dee22d3fb2e98476d3bf0973e
maksimod <58116860+maksimod@users.noreply.github.com>
2024-07-03 00:03:50 +0300

    fixed enter error

--- consoleos.py
+++ consoleos.py
@@ -3,17 +3,15 @@ import keyboard
 import time
 import requests
 import shutil
-
 import random
 import getpass
 import ast
-import string
 import re
-
 import base64
 
-from bs4 import BeautifulSoup
+from time import sleep
 
+from bs4 import BeautifulSoup
 
 # Function to provide options and handle user input
 def Option(question: str, options: list) -> str:
@@ -26,41 +24,51 @@ def Option(question: str, options: list) -> str:
                 print('>', option)
             else:
                 print(option)
-
+        print('\n')
         time.sleep(0.01)
-        
+
         if keyboard.is_pressed('down') and not x + 1 > len(options) - 1:
             x += 1
             time.sleep(0.1)
         elif keyboard.is_pressed('up') and not x - 1 < 0:
             x -= 1
             time.sleep(0.1)
-        elif keyboard.is_pressed('shift'):
-            time.sleep(0.1)  # Wait briefly to avoid multiple key presses
-            while keyboard.is_pressed('shift'):  # Wait until 'shift' is released
-                time.sleep(0.01)
-            return options[x]
+        elif keyboard.is_pressed('shift') or keyboard.is_pressed('enter'):
+            # delVar = 0
+            # while (delVar<1000000): delVar+=1
+            if (keyboard.is_pressed('shift') or keyboard.is_pressed('enter')):
+                # time.sleep(0.2)  # Wait briefly to avoid multiple key presses
+                while keyboard.is_pressed('shift') or keyboard.is_pressed('enter'):  # Wait until 'shift' is released
+                    time.sleep(0.01)
+                delVar = 0
+                while (delVar<1000000): delVar+=1
+                return options[x]
+            else:
+                continue
+        elif keyboard.is_pressed('esc'): return False
 
 # Function to handle kernel errors
 def KernelError():
     return Option("Kernel Error!", ['Restart', 'Shutdown'])
 
+
 print("Welcome to Console.os.")
 
 import os
+
 isitok = True
-apps = ['testingapp','File Manager', 'Settings', 'Shutdown', 'the virus', 'ConsoleBrowse', 'Terminal']
+apps = ['testingapp', 'File Manager', 'Settings', 'Shutdown', 'the virus', 'ConsoleBrowse', 'Terminal']
 print(os.getcwd())  # prints the current working directory
 # adjust the path as needed
 
 # Directory for data storage
-data = r'C:/Users/' + getpass.getuser() + r'/ConsoleOs' r'/ConsoleOsData'
+data = r'C:/Users/' + getpass.getuser() + r'/ConsoleOsData'
 datafound = False
-datasaved = {'Useless':'','History':[],'Balance':0}
+datasaved = {'Useless': '', 'History': []}
 if not os.path.exists(data):
     print("Data not Found. Creating Directory...")
-
     os.makedirs(data)
+
 # Function to list files in a directory
 def list_files_in_directory(directory):
     try:
@@ -71,7 +79,6 @@ def list_files_in_directory(directory):
     except PermissionError:
         print("You do not have permission to access this directory")
 
-        # Check if data file exists and load data
 for file in list_files_in_directory(data):
     if file == 'donotdelete.txt':
         datafound = True
@@ -80,9 +87,7 @@ for file in list_files_in_directory(data):
             read = file.read()
             # Split the string into key-value pairs
             datasaved = ast.literal_eval(read)
-time.sleep(1)
-
-
+            print(datasaved)
 
 if not datafound:
     print("Data not found... Writing new file...")
@@ -92,21 +97,19 @@ if not datafound:
     print(f"File '{file_path}' created.")
 
 
-
 # Function to handle system operations
 def System():
-    
     while True:
         app = Option('Choose an app:', apps)
         print(f"Opening {app}..")
         if app == 'testingapp':
-            a = Option("WHY ARE YOU HERE",['a','b'])
+            a = Option("WHY ARE YOU HERE", ['a', 'b'])
             if a == 'b':
                 x = input("Write to data... > ")
                 datasaved['Useless'] = x
             else:
-                Option(datasaved['Useless'],['GET OUT!'])
-            
+                Option(datasaved['Useless'], ['GET OUT!'])
+
         if app == 'Settings':
             setting = Option('Settings', ['BuildModule'])
             if setting == 'BuildModule':
@@ -114,136 +117,66 @@ def System():
         elif app == 'Terminal':
             os.system('cls')
             inp = input("> ")
-            install = re.search(r'\$sudo inst \;(.+)',inp)
-            package = re.search(r'\$sudo instpackage \;(.+)',inp)
+            install = re.search(r'\$sudo inst \;(.+)', inp)
             if install:
-                appsincloud = ['appstore','10110','$sudo', 'THEPACKAGE']
+                appsincloud = ['appstore']
                 appinstalling = install.group(1)
                 if appinstalling in appsincloud and not appinstalling in apps:
-                    for i in range(1,100):
-                        print("Installing..")
-                        print('[','#'*i,'0'*(100-i),']')
-                        time.sleep(random.uniform(0.01,0.1))
+                    for i in range(1, 100):
+                        print("Installing...")
+                        print('[', '#' * i, '0' * (100 - i), ']')
                         os.system('cls')
+                        time.sleep(random.uniform(0.01, 0.1))
                     apps.append(appinstalling)
-            elif package:
-                appsincloud = ['consoleoscoder']
-                appinstalling = package.group(1)
-                if appinstalling in appsincloud and not appinstalling in apps:
-                    for i in range(1,100):
-                        print("Installing Package..")
-                        print("this may take long.")
-                        print('[','#'*i,'0'*(100-i),']')
-                        time.sleep(random.uniform(0.01,0.2))
-                        os.system('cls')
-                    apps.append(appinstalling)
-
             else:
                 print("Failed to install app: app not found")
                 time.sleep(3)
-        elif app == '$sudo':
-            x = 0
-            while True:
-                os.system('cls')
-                x += 1
-                nameer = string.ascii_letters
-                randomname = ''
-                for _ in range(4):
-                    randomname += random.choice(nameer)
-                
-                print(f'$sudo inst ;{randomname}')
-                time.sleep(1)
-                if x == 30:
-                    Option("Paitience.",[''])
-                    break
-            if not 'Paitience' in apps:
-                apps.append('Paitience')
 
-        elif app == 'THEPACKAGE':
-            alphabet = 'λß©∂€ҒḡҺ!jкℓм∩оρq®ƨ†μνωχ¥z'
-            alpha = {
-                'a': 'λ',
-                'b': 'ß',
-                'c': '©',
-                'd': '∂',
-                'e': '€',
-                'f': 'Ғ',
-                'g': 'ḡ',
-                'h': 'Һ',
-                'i': '!',
-                'j': 'j',
-                'k': 'к',
-                'l': 'ℓ',
-                'm': 'м',
-                'n': '∩',
-                'o': 'о',
-                'p': 'ρ',
-                'q': 'q',
-                'r': '®',
-                's': 'ƨ',
-                't': '†',
-                'u': 'μ',
-                'v': 'ν',
-                'w': 'ω',
-                'x': 'χ',
-                'y': '¥',
-                'z': 'z'
-            }
-            e = input('import a string of txt for only using letters in the alphabet.')
-            strtoprint = ''
-            for letter in e:
-                if letter in alpha:
-                    strtoprint += f'{alpha[letter]}'
-                else:
-                    strtoprint += letter
-            Option(strtoprint,['Exit'])
-        elif app == 'Paitience':
-            x = 600
-            while x > 0:
-                os.system('cls')
-                print(f"Just wait for {x} more seconds. you'll be fine.")
-                time.sleep(1)
-                x -= 1
         elif app == 'File Manager':
-            filemanage = Option("File Manager", ['Write File','Open File'])
+            filemanage = Option("File Manager", ['Write File', 'Open File','ls'])
             time.sleep(0.4)
             if filemanage == 'Open File':
-                name = input('Filepath > ')
+                input()
+                input()
+                name = input("Enter file name >")
+
+
                 if not name == '>':
                     if name.startswith('"') and name.endswith('"'):
                         name = name[1:-1]
                     try:
                         with open(name, 'r') as file:
                             text = file.read()
-                            Option(text,['Exit'])
+                            Option(text, ['Exit'])
                     except Exception as e:
-                        Option("There was a error that happened when opening the file. so we saved you from getting a console error.",['Exit'])
-
+                        time.sleep(1)
+                        Option(
+                            "There was a error that happened when opening the file. so we saved you from getting a console error.",
+                            ['Exit'])
                 else:
-                    Option("...",['Haha'])
-                    Option("Its not funny lil bro",['and?'])
-                    if not Option("Do you want your death?",['What you gonna do?','im sorry..']) == 'im sorry..':
+                    Option("...", ['Haha'])
+                    Option("Its not funny lil bro", ['and?'])
+                    if not Option("Do you want your death?", ['What you gonna do?', 'im sorry..']) == 'im sorry..':
                         print("ALRIGHT FINE.")
                         time.sleep(3)
                         KernelError()
                     else:
                         print("Its not ok.")
                         time.sleep(4)
-                        datasaved['Useless'] = 'ITS NOT OK '*1000
+                        datasaved['Useless'] = 'ITS NOT OK ' * 1000
                         isitok = False
 
-               
-            if filemanage == 'Write File':
+            elif filemanage == 'Write File':
                 name = input("Name > ")
                 text = input("Text > ")
                 file_path = os.path.join(data, name + '.txt')
-                
+
                 # Write text to file
                 with open(file_path, 'w') as file:
                     file.write(text)
-                
+
                 print(f"File '{name}.txt' saved to '{data}'")
-                
+
         elif app == 'Shutdown':
             print("Saving data...")
             file_path = os.path.join(data, 'donotdelete.txt')
@@ -254,17 +187,7 @@ def System():
             print("Shutting down")
             print("Goodbye!")
             break
-        elif app == '10110':
-            Option("01010111 01001000 01011001 00100000 01000100 01001001 01000100 00100000 01011001 01001111 01010101 00100000 01000100 01001111 00100000 01010100 01001000 01001001 01010011",['winwin'])
-            Option("im removing all your ram.",['nooo'])
-            x = 100
-            while True:
-                if not x == 0:
-                    Option("THE LEAKRS ARE COMING",[f'NO PLS! ILL GIVE YOU {x} COOKIES!'])
-                else:
-                    Option("the gods has savd u from the leakrs!",['what?'])
-                    break
-                x -= 1
+
         elif app == 'the virus':
             print("SYSTEMOS has stopped responding.")
             time.sleep(1)
@@ -277,86 +200,19 @@ def System():
                 time.sleep(2)
                 continue
         elif app == 'appstore':
-            x = Option("Install App:",['base64 translator','Lottery!'])
-            
+            x = Option("Install App:", ['base64 translator'])
             if x == 'base64 translator':
-                for i in range(1,100):
+                for i in range(1, 100):
                     print("Installing...")
-                    print('[','#'*i,'0'*(100-i),']')
+                    print('[', '#' * i, '0' * (100 - i), ']')
                     os.system('cls')
-                    time.sleep(random.uniform(0.01,0.1))
-                    if not x in apps:
-                        apps.append(x)
-            elif x == 'Lottery!':
-                for i in range(1,100):
-                    print("Installing...")
-                    print('[','#'*i,'0'*(100-i),']')
-                    
-                    time.sleep(random.uniform(0.01,0.1))
-                    os.system('cls')
+                    time.sleep(random.uniform(0.01, 0.1))
                     if not x in apps:
                         apps.append(x)
         elif app == 'base64 translator':
             inp = input("> ")
-            x = bytes(inp,'utf-8')
-            Option(f"Base64: {str(base64.b64encode(x))}",['exit'])
-
-
-
-
-        elif app == 'Lottery!':
-            while True:
-                try:
-                    inp = int(input("Bet: "))
-                    break
-                except Exception as e:
-                    print("error.")
-
-            turn = 1
-            symbols = ['$','%','&']
-            x = '$'
-            y = '$'
-            z = '$'
-            os.system('cls')
-            for _ in range(3):
-                lastsymbol = ''
-                for _ in range(random.randint(10,100)):
-                    print(f'{x}|{y}|{z}')
-                    while True:
-                        if turn == 1:
-                            x = random.choice(symbols)
-                        elif turn == 2:
-                            y = random.choice(symbols)
-                        else:
-                            z = random.choice(symbols)
-                        if turn == 1:
-                            if not x == lastsymbol:
-                                break
-                        if turn == 2:
-                            if not y == lastsymbol:
-                                break
-                        if turn == 3:
-                            if not z == lastsymbol:
-                                break
-                    time.sleep(0.1)
-                    os.system('cls')
-                turn += 1
-            print(f'{x}|{y}|{z}')
-            time.sleep(1)
-            os.system('cls')
-                    
-
-            if x == y and y == z:
-                Option("You win!",['yay'])
-                datasaved['Balance'] += inp
-            else:
-                Option("You lose!",['Ya- wait, what?!'])
-                datasaved["Balance"] -= inp
-
-
-
-
-
+            x = bytes(inp, 'utf-8')
+            Option(f"Base64: {str(base64.b64encode(x))}", ['exit'])
 
         elif app == 'ConsoleBrowse':
             if isitok:
@@ -369,28 +225,25 @@ def System():
                     else:
                         break
 
-                
                 browse = input("Website >")
                 url = browse.strip()
 
-                
                 if not url.startswith("http://") and not url.startswith('https://'):
                     url = 'https://' + url
-                
+
                 # Replace ':' with '.' before storing in history
 
-                
                 response = fetch_webpage(url)
                 if response:
                     parsed_content = parse_html(response)
                     print(parsed_content)
                     datasaved['History'].append(url)
-                
+
                 input("Press Enter to continue...")
                 continue
             else:
-                Option("YOU CANT GET AWAY. "*20,['alright thats it'])
-                Option("OHHH MY GOOOOOo- THIS IS IMPOSSIBLE.",['noob'])
+                Option("YOU CANT GET AWAY. " * 20, ['alright thats it'])
+                Option("OHHH MY GOOOOOo- THIS IS IMPOSSIBLE.", ['noob'])
                 isitok = True
 
 
@@ -404,13 +257,14 @@ def fetch_webpage(url):
         print(f"Error: {e}")
         return None
 
+
 # Function to parse HTML content using BeautifulSoup
 def parse_html(content):
     soup = BeautifulSoup(content, 'html.parser')
     text = soup.get_text()
-    text = text.replace('\n',' ')  # Remove newlines
+    text = text.replace('\n', ' ')  # Remove newlines
     return text
+
 
 # Start the console OS system
 System()
-
